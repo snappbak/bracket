b2bd1e8977637fdbfa61c76598ecd099182c58f5
Sebastian Njose <sebastian_njose@hotmail.com>
2020-12-23 06:07:38 +0300

    Tweak

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css'
-// import Home from './Home'
+import Home from './Home'
 // import Product from './Product'
-import Bracket from './Bracket'
+// import Bracket from './Bracket'
 
 function App() {
   return (
-    <Bracket />
+    <Home />
   )
 }
 
